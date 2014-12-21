7a9f11305bd8082f444ffc17a3fe2c7dd14f235e
root <Ryan Cole>
2014-12-21 18:49:51 +1100

    update files

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+$hostname = "127.0.1";
+$user = "ryan";
+$password = "felix123";
+$dbname = "firstdatabase";
+
+// Connect to database
+$dbConnected = mysql_connect($hostname, $user, $password, $dbname);
+if(!$dbConnected) {
+	// Return Failure Message If Connection Failed
+	echo "failed: " . mysql_error();
+} else {
+	// Return Success Message If Connection Succeeded
+	echo "Successfully connected to " . $dbname . ".";
+}
+
+$sqlq = "DROP TABLE firstdatabase.testtable";
+if(!mysql_query($sqlq)) {
+	echo "failed: " . mysql_error();
+} else {
+	echo "successfully dropped table";
+}
+
+// Close Connection After Scrip Has Finnished
+mysql_close();
+?>
